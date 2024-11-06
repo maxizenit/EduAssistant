e7a91564b1308175d783ab448d49d03f1cd1f46b
romankuryshev <romankuryshev1406@gmail.com>
2024-11-06 13:36:35 +0300

    feat: add user controller

--- backend/src/main/java/ru/itmo/eduassistant/backend/repository/UserRepository.java
+++ backend/src/main/java/ru/itmo/eduassistant/backend/repository/UserRepository.java
@@ -3,5 +3,9 @@ package ru.itmo.eduassistant.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.itmo.eduassistant.backend.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByTelegramId(Long telegramId);
 }
---
+++ commons/src/main/java/ru/itmo/eduassistant/commons/dto/user/UserRole.java
@@ -0,0 +1,6 @@
+package ru.itmo.eduassistant.commons.dto.user;
+
+public enum UserRole {
+    STUDENT,
+    TEACHER
+}
