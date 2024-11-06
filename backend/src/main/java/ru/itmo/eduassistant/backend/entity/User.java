cf4a890b34b14d08b9b1327bf6576cedab007b7d
Timofey Myznikov <reedtztim2002@gmail.com>
2024-11-06 15:23:01 +0300

    Поправил telegram_id

--- backend/src/main/java/ru/itmo/eduassistant/backend/entity/User.java
+++ backend/src/main/java/ru/itmo/eduassistant/backend/entity/User.java
@@ -16,6 +16,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "telegram_id")
     private Long telegramId;
 
     private String username;
